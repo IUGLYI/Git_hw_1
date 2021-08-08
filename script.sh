948829beccb8d28ada8e312b8d9a3440b936d1c5
IUGLYI <stanislav.plekhanov@gmail.com>
2021-08-08 11:33:16 +0300

    new

---
+++ script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd /C/Users/stani/git/Git_hw_1
+mkdir QA1 QA2 QA3
+cd QA1
+touch qa1.txt qa2.txt qa3.txt qa1.json qa2.json
+mkdir QA4 QA5 QA6
+ls
+mv /C/Users/stani/git/Git_hw_1/QA1/qa1.json /C/Users/stani/git/Git_hw_1/QA1/qa2.json /C/Users/stani/git/Git_hw_1/QA2
+
+
